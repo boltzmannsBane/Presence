ac9045acbe7255b3048bf61d674d41a741ef6aa8
Ral Mayer <llev4n@gmail.com>
2019-11-12 16:25:35 +0400

    working on authentication

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,16 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { Header } from './Header'
 
-export const Profile = () => {
+export const Profile = ({ match: { params: { id } } }) => {
     
-    
+    useEffect(() => {
+        console.log(id)
+    })
+
     return <Fragment>
         <h1>Profile Component</h1>
-        <Header />
-        
+        <Header id={id}/>
+
     </Fragment>
 
 
