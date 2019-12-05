7f7376c939eba31065aa1c3a0c70bfb6bafeaf95
Ral Mayer <llev4n@gmail.com>
2019-12-05 21:22:49 +0400

    gallery styling fixed

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -2,6 +2,6 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 
 export const SubmitButton = ({values, elementName}) =>  <Button type='submit' variant="contained" color={
-    elementName==='gallery' && values.images.length <= 0 ? 'disabled' : 'primary'} style={{marginTop: '20px'}}>
+    elementName==='gallery' && values.images.length <= 0 ? 'default' : 'primary'} style={{marginTop: '20px'}}>
 Submit
 </Button>
